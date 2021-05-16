e20850be9ef719be6bb9555d5ec610831fd21d95
cryptosig <7476547+cryptosig@users.noreply.github.com>
2021-05-15 18:02:18 -0700

    Initial Commit

---
+++ vendor/umd.js
@@ -0,0 +1,13 @@
+(function(root, factory) {
+  if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+    /* global require */
+    factory(exports);
+  } else {
+    root.peerplaysio = root.peerplaysio || {};
+    factory((root.peerplaysio.sjcl = {}));
+  }
+})(this, function(exports) {
+
+  // INSERT
+
+});
